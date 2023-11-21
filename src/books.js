fdc01e20c715b88d877ffe5592c703ac669ceaf9
Vincent <141185796+Yalcael@users.noreply.github.com>
2023-11-21 18:32:42 +0100

    push book react project

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+    {
+      id: 1,
+      img: "https://m.media-amazon.com/images/I/41HKdOUR4jL._SY445_SX342_.jpg",
+      title: "Forget Me Not",
+      author: "Danielle Lemaire"
+    },
+  
+    {
+      id: 2,
+      img: "https://m.media-amazon.com/images/I/51rnGCwdflL._SY445_SX342_.jpg",
+      title: "Le voyage de Shuna",
+      author: "Hayao Miyazaki"
+    },
+  
+    {
+      id: 3,
+      img: "https://m.media-amazon.com/images/I/41zzPJXtNvL._SY445_SX342_.jpg",
+      title: "Son odeur après la pluie",
+      author: "Cédric Sapin-Defour"
+    },
+  
+  ];
